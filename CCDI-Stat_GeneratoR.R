745d38368e79f77af11a12d3421950c3de6bbc03
svburke <sean.burke2@nih.gov>
2023-03-13 14:36:10 -0500

    Update CCDI-Stat_GeneratoR.R

--- CCDI-Stat_GeneratoR.R
+++ CCDI-Stat_GeneratoR.R
@@ -64,7 +64,7 @@ option_list = list(
 )
 
 #create list of options and values for file input
-opt_parser = OptionParser(option_list=option_list, description = "\nCCDI-Stat_GeneratoR v2.0.0")
+opt_parser = OptionParser(option_list=option_list, description = "\nCCDI-Stat_GeneratoR v2.0.1")
 opt = parse_args(opt_parser)
 
 #If no options are presented, return --help, stop and print the following message.
